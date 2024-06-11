f2f7823bc8286faed77ba597796735de9e984333
Alex <31933549+crypt096@users.noreply.github.com>
2024-06-11 17:34:20 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
+import { Command } from 'commander';
+import * as fs from 'fs';
 import { Pathfinder } from './pathfinder.class';
 
-const exampleMap = `
-  @-A-+-B-x
-`;
+const program = new Command();
+
+program
+  .option('-f, --file <path>', 'file path')
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (!options.file) {
+  console.error('Error: You must specify a file using the -f or --file option.');
+  process.exit(1);
+}
 
 try {
-        const pathfinder = new Pathfinder(exampleMap);
-        console.log(`Letters: ${pathfinder.UniquePathCharacters}`);
-        console.log(`Path as characters: ${pathfinder.PathString}`);
+  const content = fs.readFileSync(options.file, { encoding: 'utf8' });
+  const pathfinder = new Pathfinder(content);
+  console.log(`Path: ${pathfinder.PathString}`);
+  console.log(`Collected letters: ${pathfinder.UniquePathCharacters}`);
 } catch (error) {
-        console.log(error);
+  console.error(`Error: ${error.message}`);
+  process.exit(1);
 }
